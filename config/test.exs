2a95b373f84d9e82249e8d9df1f88a6c77b11e8c
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-04-22 23:46:31 -0700

    INIT: Blank project

---
+++ config/test.exs
@@ -0,0 +1,34 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :example, Example.Repo,
+  database: Path.expand("../example_test.db", __DIR__),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :example, ExampleWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "OqkjwczdtQBWgeTB1FuYjnTnASpMYfCVn7jgkeGfLeMSmOiM8RbXM64P1UiEikJ2",
+  server: false
+
+# In test we don't send emails
+config :example, Example.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
